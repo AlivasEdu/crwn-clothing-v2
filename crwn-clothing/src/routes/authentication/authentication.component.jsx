a1e449bbae7d8557dc55f131cdd3d20dcb425f3e
AlivasGR <alivanidis@gmail.com>
2026-02-28 17:57:17 +0200

    styled components changes, links on home page, minor cleanup

--- crwn-clothing/src/routes/authentication/authentication.component.jsx
+++ crwn-clothing/src/routes/authentication/authentication.component.jsx
@@ -3,41 +3,15 @@ import { useState } from "react";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
-import "./authentication.styles.scss";
-
-const defaultFormFields = {
-  email: "",
-  password: "",
-};
+import { AuthenticationContainer } from "./authentication.styles.jsx";
 
 const Authentication = () => {
-  const [formFields, setFormFields] = useState(defaultFormFields);
-  const {  email, password } = formFields;
-  
-  const resetFormFields = () => {
-    setFormFields(defaultFormFields);
-  };
-  
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-    setFormFields({ ...formFields, [name]: value });
-  };
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    try {
-      resetFormFields();
-    } catch (error) {
-      console.log("user sign in failed", error);
-    }
-  };
-
-  
   return (
-    <div className="authentication-container">
+    <AuthenticationContainer>
       <SignInForm />
       <SignUpForm />
-    </div>
+    </AuthenticationContainer>
   );
 };
 
